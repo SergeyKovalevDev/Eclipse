4319a695af1097d925b5145c9c935cf9b773fc8e
Sergey Kovalev <dip.deep@mail.ru>
2022-02-05 23:31:27 +0300

    modified Main.java

--- src/taskPackage/Main.java
+++ src/taskPackage/Main.java
@@ -1,10 +1,29 @@
 package taskPackage;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		taskStarter();
+	}
+	
+	private static void taskStarter() {
+		List<Animal> animalList = new ArrayList<>();
+		animalList.add(new Cat("awww", 5, LocalDateTime.now()));
+		animalList.add(new Cat("bwww", 15, LocalDateTime.now()));
+		animalList.add(new Cat("cwww", 7, LocalDateTime.now()));
+		collectionPrinter(animalList);
+		
+	}
+	
+	private static void collectionPrinter(Collection<Animal> collection) {
+		for (Animal element : collection) {
+			System.out.println(element);
+		}
 	}
 
 }
