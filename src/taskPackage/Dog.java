0338d237cec4dda156d08744083594ee620761b8
Sergey Kovalev <dip.deep@mail.ru>
2022-02-05 22:54:02 +0300

    modified Dog.java

--- src/taskPackage/Dog.java
+++ src/taskPackage/Dog.java
@@ -1,5 +1,11 @@
 package taskPackage;
 
-public class Dog {
+import java.time.LocalDateTime;
+
+public class Dog extends Animal {
+
+	public Dog(String breed, String name, Integer age, LocalDateTime creationDate) {
+		super(breed, name, age, creationDate);
+	}
 
 }
