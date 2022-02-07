15f43fd50f4d1689ebcd0dccf7220e64da4e2f8e
Sergey Kovalev <dip.deep@mail.ru>
2022-02-07 14:35:13 +0300

    modified Main.java

--- src/taskPackage/Main.java
+++ src/taskPackage/Main.java
@@ -2,7 +2,6 @@ package taskPackage;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
